fddb246af44afc59fa8b321e7d14bcbe5db2ff80
francois80 <glavieux.francois@orange.fr>
2019-11-05 10:50:03 +0100

    ajout exercice 4 jquery partie 2

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('#green').click(function(){
+    $('#text').css('color','green');
+  });
+
+  $('#red').click(function(){
+    //alert('coucou');
+    $('#text').css('color','red');
+  });
+
+  $('#blue').click(function(){
+    //alert('coucou');
+    $('#text').css('color','blue');
+  });
+
+});
